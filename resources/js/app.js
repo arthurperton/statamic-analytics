eaf17fe7b5bdd22f25ea5a0d0c7a0864d53ee725
Arthur Perton <arthur@webenapp.nl>
2023-08-15 10:46:07 +0200

    Add analytics js package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-alert('Hello from the Statalytics addon!')
+import Analytics from 'analytics'
+
+/* Initialize analytics */
+const analytics = Analytics({
+  app: 'Statalytics',
+//   version: 100,
+//   plugins: []
+})
+
+/* Track a page view */
+analytics.page()
+
+// analytics.identify(new Date().toISOString())
+
+analytics.on('page', (event) => {
+    console.log('page event', event)
+    // console.log('payload', payload)
+  })
