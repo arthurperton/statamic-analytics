6ed1c9e855f3db8cedfc4b479e03542803097483
Arthur Perton <arthur@webenapp.nl>
2023-08-25 11:31:42 +0200

    Remove unnecessary code

--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -11,27 +11,7 @@ class DashboardController extends CpController
 {
     public function index()
     {
-        $to = Carbon::now(); //->startOfDay();
-        $from = $to->clone()->subDays(7);
-
-        $visitors = StatsHelper::uniqueVisitors($from, $to);
-        $chart = StatsHelper::uniqueVisitorsChart($from, $to);
-        $visits = StatsHelper::visits($from, $to);
-        $pageviews = StatsHelper::pageviews($from, $to);
-        $views = StatsHelper::viewsPerVisit($from, $to);
-        $duration = StatsHelper::visitDuration($from, $to);
-        $sources = StatsHelper::sources($from, $to);
-        $pages = StatsHelper::pages($from, $to);
-        $locations = StatsHelper::locations($from, $to);
-        $browsers = StatsHelper::browsers($from, $to);
-        $operatingSystems = StatsHelper::operatingSystems($from, $to);
-        $devices = StatsHelper::devices($from, $to);
-
-        return view('analytics::dashboard', compact(
-            'chart',
-            'visitors', 'visits', 'pageviews', 'views', 'duration', 'sources',
-            'pages', 'locations', 'browsers', 'operatingSystems', 'devices',
-        ));
+        return view('analytics::dashboard');
     }
 
     public function stats(Request $request)
