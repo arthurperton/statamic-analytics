ff081578f0ec2aee072b14d701df65967725e064
Arthur Perton <arthur@webenapp.nl>
2023-11-29 12:03:37 +0100

    Fix js issues for chart using Alpine

--- resources/views/livewire/trend-chart.blade.php
+++ resources/views/livewire/trend-chart.blade.php
@@ -1,10 +1,9 @@
-<div class="shadow-md bg-white rounded-xl px-4 pt-4">
-
-    @script
-    <script type="text/javascript">
-        const option = {
+<div 
+    class="shadow-md bg-white rounded-xl px-4 pt-4"
+    x-data="{
+        chart: undefined,
+        option: {
             animation: false,
-            // animationDuration: 200,
             grid: {
                 top: 64,
                 bottom: 64,
@@ -25,54 +24,40 @@
                 {
                     type: 'line',
                     smooth: false,
-                    // color: '#0AD7B0', 
                     color: '#2D9CF9',
                     areaStyle: {
-                        // color: '#0AD7B0',
                         color: '#2D9CF9',
                         opacity: 0.2,
                     },
                 },
             ],
-        };
+        },
+        setChartData(data) {
+            this.option.xAxis.data = data.map(d => d.timestamp)
+            this.option.series[0].data = data.map(d => d.value)
 
-        const chart = echarts.init(document.getElementById('chart'));
+            this.chart.setOption(this.option)
+        },
+        toggleSmooth() {
+            this.option.series[0].smooth = ! this.option.series[0].smooth
+
+            this.chart.setOption(this.option)
+        },
+    }"
+    x-init="
+        chart = echarts.init(document.getElementById('chart'))
+
         window.addEventListener('resize', function() {
-            chart.resize();
+            chart.resize()
         });
-
-        function setChartData(data) {
-            
-            // option.title = { text: 'Unique Visitors' }
-            option.xAxis.data = data.map(d => d.timestamp)
-            option.series[0].data = data.map(d => d.value)
-
-            chart.setOption(option);
-        }
-
-        function toggleSmooth() {
-            option.series[0].smooth = ! option.series[0].smooth
-            chart.setOption(option)
-        }
 
         document.addEventListener('livewire:initialized', async () => {
             setChartData(await $wire.data())
 
-            $wire.$on('data', ([data]) => {
-                // console.log('rendered')
-                console.log('data')
-
-                setTimeout(() => {
-                 
-                    setChartData(data)
-                    // console.log(data)
-
-                }, 0);
-            });
-        });
-
-    </script>
-    @endscript
+            $wire.$on('data', ([data]) => setTimeout(() => setChartData(data), 0))
+        })
+    "
+>
     
     <div class="flex">
         @include('analytics::component.button-group', [
@@ -81,7 +66,8 @@
 
         <button 
             @click.prevent="toggleSmooth()"
-            class="ml-4 px-2 py-0.5 border border-slate-300 rounded-lg text-sm font-light"
+            class="ml-4 px-2 py-0.5 border border-slate-300 rounded-lg text-sm"
+            :class="option.series[0].smooth ? 'shadow-inner bg-slate-50 font-medium' : 'font-light'"
         >smooth</button>
     </div>
 
