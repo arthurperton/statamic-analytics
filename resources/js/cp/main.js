fdfd19dc131a677ff153841974e7d9b5e1ae2c13
Arthur Perton <arthur@webenapp.nl>
2023-11-30 15:14:14 +0100

    Remove dead code

--- resources/js/cp/main.js
+++
@@ -1,7 +0,0 @@
-import Dashboard from './components/Dashboard';
-import ResizeContainer from './components/ResizeContainer';
-
-Statamic.booting(() => {
-    Statamic.$components.register('resize-container', ResizeContainer);
-    Statamic.$components.register('analytics-dashboard', Dashboard);
-});
