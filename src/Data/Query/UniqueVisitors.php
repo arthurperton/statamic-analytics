1bf708c6d880387fe2db0fed38b7754517d1ef04
Arthur Perton <arthur@webenapp.nl>
2023-12-01 10:02:08 +0100

    Use session_started_at

--- src/Data/Query/UniqueVisitors.php
+++ src/Data/Query/UniqueVisitors.php
@@ -11,8 +11,8 @@ class UniqueVisitors extends AbstractQuery
         return Database::connection()
             ->table('v_pageview')
             ->selectRaw('count(DISTINCT anonymous_id) AS value')
-            ->where('started_at', '>=', $this->from->getTimestamp())
-            ->where('started_at', '<', $this->to->getTimestamp());
+            ->where('session_started_at', '>=', $this->from->getTimestamp())
+            ->where('session_started_at', '<', $this->to->getTimestamp());
     }
 
     protected function fetchData()
