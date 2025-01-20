94456e53b9cc491c138bfe9bd84797ffb41697f7
Arthur Perton <arthur@webenapp.nl>
2025-01-20 12:51:54 +0100

    Update package.json and webpack to use react and the latesta version of tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,9 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
 
-mix
-    .setPublicPath('dist')
+mix.setPublicPath('dist')
     .postCss('resources/css/cp.css', 'dist/css/cp.css', [
         require('tailwindcss'),
     ])
     .js('resources/js/cp/main.js', 'dist/js/cp.js')
     .js('resources/js/web/main.js', 'dist/js/web.js')
-    .vue({ version: 2 });
+    .react()
